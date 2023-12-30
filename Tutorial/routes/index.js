74dee6713b3eba2fd6f569591e94b33f2db1e8b3
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2023-12-29 22:00:42 -0800

    create file for deployment

---
+++ Tutorial/routes/index.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+
+//create an array called messages
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date(),
+  });
+  console.log(messages);
+  res.redirect("/");
+});
+
+module.exports = router;
